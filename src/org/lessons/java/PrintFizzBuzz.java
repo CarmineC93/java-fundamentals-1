997c49f2c9f11fd60df6ecce9c2c92c3156d0e58
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-03-08 15:33:57 +0100

    FizzBuzz

---
+++ src/org/lessons/java/PrintFizzBuzz.java
@@ -0,0 +1,30 @@
+package org.lessons.java;
+
+public class PrintFizzBuzz {
+    public static void main(String[] args) {
+
+/*        Consegna: Scrivi un programma che stampi i numeri da 1 a 100,
+            ma
+        per i multipli di 3 stampi “Fizz” al posto del numero e
+        per i multipli di 5 stampi “Buzz”.
+        Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+        Esempio: 1 2 Fizz 4 Buzz Fizz 7 8 Fizz……
+        Bonus: invece che 100, chiedere all’utente quanti numeri vuole stampare
+                (il valore inserito dall’utente deve essere maggiore di 0 e minore di 1000)*/
+
+        for (int i = 1; i <= 100; i++){
+            if (i % 5 == 0 && i % 3 == 0 ){
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 5 == 0 ){
+                System.out.println("Buzz");
+            }
+            else if( i % 3 == 0 ){
+                System.out.println("Fizz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
